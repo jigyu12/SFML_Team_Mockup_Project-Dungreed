e8f15f32083fca6903f14eb12f3301d2aac7ef0e
박지광 <pdk00137@gmail.com>
2024-11-26 18:49:28 +0900

    플레이어 헤더추가

---
+++ Team_Mockup_Project-Dungreed/Player.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "Character.h"
+class Player : public Character
+{
+protected:
+	sf::Vector2f velocity;
+
+	bool isJumping;
+	bool isGrounded;
+public:
+	Player(const std::string& name = "");
+	virtual ~Player() = default;
+
+	virtual void SetPosition(const sf::Vector2f& pos) override;
+	virtual void SetRotation(float angle) override;
+	virtual void SetScale(const sf::Vector2f& scale) override;
+
+	virtual void SetOrigin(Origins preset) override;
+	virtual void SetOrigin(const sf::Vector2f& newOrigin) override;
+
+	virtual void Init() override;
+	virtual void Release() override;
+	virtual void Reset() override;
+	virtual void Update(float dt) override;
+	virtual void Draw(sf::RenderWindow& window) override;
+};
+
