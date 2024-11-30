a8e6985556e141f616db49a362e75cd0afa29292
민지규 <jigyu12@gmail.com>
2024-11-30 15:30:50 +0900

    무기 스왑 구현

--- Team_Mockup_Project-Dungreed/Player.cpp
+++ Team_Mockup_Project-Dungreed/Player.cpp
@@ -3,6 +3,7 @@
 #include "SceneDev1.h"
 #include "SceneDev3.h"
 #include "Room.h"
+#include "Weapon.h"
 
 Player::Player(const std::string& name)
 	:Character(name), velocity({ 0.f,0.f })
@@ -83,11 +84,7 @@ void Player::Reset()
 	animator.SetTarget(&body);
 	animator.Play("animations/player Idle.csv");
 	/*body.setTexture(TEXTURE_MGR.Get(playerId));*/
-	sword.setTexture(TEXTURE_MGR.Get(swordId));
-	
 
-
-	sword.setOrigin(Utils::SetOrigin(sword, Origins::BC));
 	hitbox.SetColor(sf::Color::Blue);
 
 	SetPosition({ 0.f,0.f });
@@ -125,6 +122,16 @@ void Player::SetStatus(Status status)
 
 void Player::Update(float dt)
 {
+	if (InputMgr::GetKeyDown(sf::Keyboard::Num1))
+	{
+		SwitchWeaponSlot(sf::Keyboard::Num1);
+	}
+	else if(InputMgr::GetKeyDown(sf::Keyboard::Num2))
+	{
+		SwitchWeaponSlot(sf::Keyboard::Num2);
+	}
+	
+
 	animator.Update(dt);
 
 	dashCoolTimer += dt;
@@ -159,13 +166,11 @@ void Player::Update(float dt)
 
 
 	SetPosition(position + velocity * dt);
-	sword.setPosition({ body.getPosition().x ,body.getPosition().y - 9 });
 
 
 	sf::Vector2i mousePos = InputMgr::GetMousePosition();
 	sf::Vector2f mouseworldPos = SCENE_MGR.GetCurrentScene()->ScreenToWorld(mousePos);
-	look = Utils::GetNormal(mouseworldPos - sword.getPosition());
-	sword.setRotation(Utils::Angle(look) + 90);
+	look = Utils::GetNormal(mouseworldPos - position);
 	SetOrigin(Origins::BC);
 
 
@@ -332,8 +337,6 @@ void Player::UpdateDash(float dt)
 	}
 }
 
-
-
 void Player::Jump()
 {
 	velocity.y = -jumpForce;
@@ -341,14 +344,37 @@ void Player::Jump()
 	SetStatus(Status::Jump);
 }
 
+void Player::SetWeaponToWeaponSlot1(Weapon* weapon, bool isCurrentWeapon)
+{
+	weaponSlot1 = weapon;
+	weaponSlot1->SetIsCurrentWeapon(isCurrentWeapon);
+}
 
+void Player::SetWeaponToWeaponSlot2(Weapon* weapon, bool isCurrentWeapon)
+{
+	weaponSlot2 = weapon;
+	weaponSlot2->SetIsCurrentWeapon(isCurrentWeapon);
+}
 
+void Player::SwitchWeaponSlot(sf::Keyboard::Key key)
+{
+	if (key != sf::Keyboard::Num1 && key != sf::Keyboard::Num2)
+		return;
+
+	if (key == sf::Keyboard::Num1)
+	{
+		weaponSlot1->SetIsCurrentWeapon(true);
+		weaponSlot2->SetIsCurrentWeapon(false);
+	}
+	else
+	{
+		weaponSlot1->SetIsCurrentWeapon(false);
+		weaponSlot2->SetIsCurrentWeapon(true);
+	}
+}
 
 void Player::Draw(sf::RenderWindow& window)
 {
 	window.draw(body);
-	window.draw(sword);
 	hitbox.Draw(window);
-
-
 }
