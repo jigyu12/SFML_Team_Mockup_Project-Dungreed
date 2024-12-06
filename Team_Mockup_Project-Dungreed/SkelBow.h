f4334ba484b33d9e892e1c9dc6059dd6e8d11d87
민지규 <jigyu12@gmail.com>
2024-12-06 23:37:53 +0900

    해골 칼 & 활 기본 리소스 추가

---
+++ Team_Mockup_Project-Dungreed/SkelBow.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "Monster.h"
+
+class SkelBow : public Monster
+{
+
+};
---
+++ Team_Mockup_Project-Dungreed/SkelBowArrow.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "SpriteGo.h"
+
+class SkelBowArrow : public SpriteGo
+{
+
+};
