dba493c5deae41ab29f0c97f5e73e4b58992cf05
황규영 <kyuy9214@gmail.com>
2024-12-08 13:21:51 +0900

    부숴진 오브젝트에서 추가 파편이 생성되는 버그 수정

--- Team_Mockup_Project-Dungreed/ShortSword.cpp
+++ Team_Mockup_Project-Dungreed/ShortSword.cpp
@@ -58,9 +58,9 @@ void ShortSword::Reset()
 	SetTextureId("graphics/weapon/Sword.png");
 	sprite.setTexture(TEXTURE_MGR.Get(textureId));
 
-	
 
-	
+
+
 
 	originalDamageMin = 8;
 	originalDamageMax = 10;
@@ -236,16 +236,17 @@ void ShortSword::Attack()
 			{
 				int realDamage = owner->CalculationDamage(GetAttackDamage());
 				monster->OnDamaged(realDamage);
-				
+
 			}
 
-			
+
 		}
 	}
 	const auto& mapObjects = ROOM_MGR.GetCurrentRoom()->GetBreakableObjects();
 	for (auto& mapObject : mapObjects)
 	{
-		if (Utils::CheckCollision(mapObject->GetHitBox(), attackBoundHitbox))
+		if (mapObject->GetStatus() != MapObject::Status::Broken
+			&& Utils::CheckCollision(mapObject->GetHitBox(), attackBoundHitbox))
 		{
 			mapObject->OnDamaged(1);
 		}
